154c081e600fc253b81b097ac7766b3997a50fbd
Ajey <37513216+AjeyOS@users.noreply.github.com>
2025-07-14 22:56:14 +0530

    Update the url

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,4 +2,4 @@ export const generateRoomId = () => {
   return Math.random().toString(36).substring(2, 8).toUpperCase();
 };
 
-export const SOCKET_URL = 'http://localhost:3000/';
+export const SOCKET_URL = 'https://streamhive-20wh.onrender.com/';
